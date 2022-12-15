76bfc84e6d3d0a275487f0558d3c02f5ace1bde4
pyLerner <lerner.v.b@gmail.com>
2022-12-15 13:15:50 +0300

    Курс валют. Добавлен заголовок и разбивка на ячейки

---
+++ main.py
@@ -0,0 +1,42 @@
+import requests
+import json
+from flask import Flask
+
+
+def get_valutes_list():
+    url = 'https://www.cbr-xml-daily.ru/daily_json.js'
+    response = requests.get(url)
+    data = json.loads(response.text)
+    valutes = list(data['Valute'].values())
+    return valutes
+
+
+app = Flask(__name__)
+
+
+def create_html(valutes):
+    text = '<h1>Курс валют</h1>'
+    text += '<table cellspacing="1" border="1" cellpadding="5" width="100" bordercolor="grey">' #отрисовка ячеек
+    text += '<tr>'
+    for key in valutes[0]:          # Добавлен залоговок
+        text += f'<th>{key}</th>'   #
+    text += '</tr>'
+    for valute in valutes:
+        text += '<tr>'
+        for v in valute.values():
+            text += f'<td>{v}</td>'
+        text += '</tr>'
+
+    text += '</table>'
+    return text
+
+
+@app.route("/")
+def index():
+    valutes = get_valutes_list()
+    html = create_html(valutes)
+    return html
+
+
+if __name__ == "__main__":
+    app.run()
